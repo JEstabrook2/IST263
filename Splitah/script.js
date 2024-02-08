cc91586f63f6d9f58272524b53a5150c1abee678
JEstabrook2 <157045325+JEstabrook2@users.noreply.github.com>
2024-02-08 15:44:46 -0500

    push

---
+++ Splitah/script.js
@@ -0,0 +1,7 @@
+document.getElementById('discoverBtn').addEventListener('click', function() {
+    window.location.href = '#introduction'; // Adjust as needed for actual navigation
+});
+
+document.getElementById('forRestaurantsBtn').addEventListener('click', function() {
+    window.location.href = '#features'; // Adjust as needed for actual navigation
+});
